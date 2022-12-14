d302d5c44acbe7f5555f79af6b0d8a7b8749522a
AbbasChallawala <abbas.challawala@civica2.com>
2022-12-14 15:13:06 +0530

    Update Startup.cs

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -32,10 +32,7 @@ namespace WishList
                 app.UseExceptionHandler("/Home/Error");
             }
             app.UseRouting();
-            //app.Run(async (context) =>
-            //{
-            //    await context.Response.WriteAsync("Hello World!");
-            //});
+            
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapDefaultControllerRoute();
